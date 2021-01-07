d473314f67c3a92cc5a5691834493be0edb70573
笠井一輝 <kazu@kazuki.local>
2021-01-07 23:00:26 +0900

    Fix: 足らないバリデーションテストの追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,31 +5,33 @@ RSpec.describe Task, type: :model do
     it 'is valid with all attributes' do
       task = create(:task)
       expect(task).to be_valid
+      expect(task.errors).to be_empty
     end
 
     it 'is invalid without title' do
       task_without_title = build(:task,title: nil)
-      task_without_title.valid?
+      expect(task_without_title).to be_invalid
       expect(task_without_title.errors[:title]). to include("can't be blank")
     end
 
     it 'is invalid without status' do
       task_without_status = build(:task,status: nil)
-      task_without_status.valid?
+      expect(task_without_status).to be_invalid
       expect(task_without_status.errors[:status]). to include("can't be blank")
     end
 
     it 'is invalid with a duplicate title' do
       task = create(:task)
-      task_duplicate_title = build(:task, title: task.title)
-      task_duplicate_title.valid?
-      expect(task_duplicate_title.errors[:title]). to include("has already been taken")
+      task_with_duplicated_title = build(:task, title: task.title)
+      expect(task_with_duplicated_title).to be_invalid
+      expect(task_with_duplicated_title.errors[:title]). to include("has already been taken")
     end
 
     it 'is valid with another title' do
-      task_another_title1 = create(:task)
-      task_another_title2 = build(:task,title: 'テスト1')
-      expect(task_another_title2).to be_valid
+      task = create(:task)
+      task_with_another_title = build(:task,title: 'テスト1')
+      expect(task_with_another_title).to be_valid
+      expect(task_with_another_title.errors).to be_empty
     end
   end
 end
