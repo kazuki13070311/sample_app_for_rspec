22b7390f28dca184e96b1b1909cd2737a2a31c2f
笠井一輝 <kazu@kazuki.local>
2021-01-07 22:32:09 +0900

    Fix: 何のテストか分かりやすくするためにグループ化を追記

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,34 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  
-  it 'is valid with all attributes' do
-    task = create(:task)
-    expect(task).to be_valid
-  end
+  describe 'validation' do
+    it 'is valid with all attributes' do
+      task = create(:task)
+      expect(task).to be_valid
+    end
 
-  it 'is invalid without title' do
-    task_without_title = build(:task,title: nil)
-    task_without_title.valid?
-    expect(task_without_title.errors[:title]). to include("can't be blank")
-  end
+    it 'is invalid without title' do
+      task_without_title = build(:task,title: nil)
+      task_without_title.valid?
+      expect(task_without_title.errors[:title]). to include("can't be blank")
+    end
 
-  it 'is invalid without status' do
-    task_without_status = build(:task,status: nil)
-    task_without_status.valid?
-    expect(task_without_status.errors[:status]). to include("can't be blank")
-  end
+    it 'is invalid without status' do
+      task_without_status = build(:task,status: nil)
+      task_without_status.valid?
+      expect(task_without_status.errors[:status]). to include("can't be blank")
+    end
 
-  it 'is invalid with a duplicate title' do
-    task = create(:task)
-    task_duplicate_title = build(:task, title: task.title)
-    task_duplicate_title.valid?
-    expect(task_duplicate_title.errors[:title]). to include("has already been taken")
-  end
+    it 'is invalid with a duplicate title' do
+      task = create(:task)
+      task_duplicate_title = build(:task, title: task.title)
+      task_duplicate_title.valid?
+      expect(task_duplicate_title.errors[:title]). to include("has already been taken")
+    end
 
-  it 'is valid with another title' do
-    task_another_title1 = create(:task)
-    task_another_title2 = build(:task,title: 'テスト1')
-    expect(task_another_title2).to be_valid
+    it 'is valid with another title' do
+      task_another_title1 = create(:task)
+      task_another_title2 = build(:task,title: 'テスト1')
+      expect(task_another_title2).to be_valid
+    end
   end
 end
