fb424f9b86c2311dfebaae5e3a53aeacc5ec9628
笠井一輝 <kazu@kazuki.local>
2021-01-08 20:13:33 +0900

    Fix: statusの文言を分かりやすい様に修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :task do
     sequence(:title, "title_1")
     content {'サンプル'}
-    status {1}
+    status { :todo }
     deadline { 1.week.from_now }
     association :user
   end
