2bd56175e0090afde10afc52d5c7f6ef9c1df481
笠井一輝 <kazu@kazuki.local>
2021-01-08 20:14:48 +0900

    Fix: スペースを追加

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :task do
     sequence(:title, "title_1")
-    content {'サンプル'}
+    content { 'サンプル' }
     status { :todo }
     deadline { 1.week.from_now }
     association :user
